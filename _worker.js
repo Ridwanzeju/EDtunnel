b94a002d5cd8e0708e329cd13cb402ecbfe8b736
Ridwanzeju <154827488+Ridwanzeju@users.noreply.github.com>
2023-12-26 09:51:46 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'a5780c9e-99d5-4946-bb87-ce2b2ddee69a';
 
-const proxyIPs = ['147.139.199.131'];
+const proxyIPs = ['104.28.154.153'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
